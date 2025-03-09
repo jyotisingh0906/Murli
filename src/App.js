1d513cf65ce0a3a519987c973606997c66f5e136
jyotisingh0906 <106544794+jyotisingh0906@users.noreply.github.com>
2025-03-09 12:35:37 +0530

    Project completed successfully

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import LetterForm from './components/LetterForm';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="App-main">
+        {/* Render the LetterForm and LetterDisplay components here */}
+        <LetterForm />
+      </main>
+    </div>
+  );
+}
+
+export default App;
